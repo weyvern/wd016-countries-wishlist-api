97d975df096223c6b362f9a98bd55a4c341be515
Jorge Paul <jserranopaul@gmail.com>
2021-07-23 14:49:47 +0200

    Get all and create (Simple)

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import express from 'express';
 import countries from './data/countries.js';
 
-console.log(countries);
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.get('/api/countries', (req, res) => res.json(countries));
+app.post('/api/countries', (req, res) => {
+  const newCountry = {
+    id: countries.length + 1,
+    ...req.body
+  };
+  countries.push(newCountry);
+  res.json(newCountry);
+});
+app.use('/', (req, res) => res.send('Welcome to Countries API'));
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
