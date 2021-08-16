b19c0ebc4487f6c95fd9af21624a2b3650538a55
Jorge Paul <jserranopaul@gmail.com>
2021-08-16 14:00:04 +0200

    CORS

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+import 'dotenv/config.js'
 import express from 'express';
+import cors from 'cors'
 import countriesRouter from './routes/countriesRouter.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors({origin: process.env.ORIGIN}));
+
 app.use('/api/countries', countriesRouter);
 app.use('/', (req, res) => res.send('Welcome to Countries API'));
 
