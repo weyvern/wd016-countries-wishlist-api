5970627fb21fb023976e0c22aea8a89e360bc890
Jorge Paul <jserranopaul@gmail.com>
2021-08-16 15:07:30 +0200

    Changed welcome msg

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import 'dotenv/config.js'
+import 'dotenv/config.js';
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import countriesRouter from './routes/countriesRouter.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({origin: process.env.ORIGIN}));
+app.use(cors({ origin: process.env.ORIGIN }));
 
 app.use('/api/countries', countriesRouter);
-app.use('/', (req, res) => res.send('Welcome to Countries API'));
+app.use('/', (req, res) => res.send('Welcome to Countries API on Heroku'));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
